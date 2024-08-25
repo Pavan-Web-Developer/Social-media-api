f848a309e26b86781e5d042f07747bbec29dbc63
Pavan <jordan.pac97@gmail.com>
2024-08-25 16:34:51 -0400

    first init

---
+++ middlewares/sendEmail.js
@@ -0,0 +1,21 @@
+const nodeMailer = require("nodemailer");
+
+exports.sendEmail = async (options) => {
+  const transporter = nodeMailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "a0187166a67f88",
+      pass: "0472653b4fd40b"
+    }
+  });
+
+  const mailOptions = {
+    from: process.env.SMPT_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
