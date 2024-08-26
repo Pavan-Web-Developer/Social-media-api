65204ef3774ecc93f1998826752543a91ce35899
Pavan <jordan.pac97@gmail.com>
2024-08-25 20:24:00 -0400

    database config

--- config/database.js
+++ config/database.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 exports.connectDatabase = () => {
   mongoose
-    .connect("mongodb+srv://jordanpac97:8gApq9kb65uiAU6q@cluster0.5vqoh.mongodb.net/")
+    .connect("mongodb+srv://jordanpac97:8gApq9kb65uiAU6q@cluster0.5vqoh.mongodb.net/", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000 // Increase the timeout to 5 seconds
+    })
     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
     .catch((err) => console.log(err));
 };
