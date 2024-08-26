785b72446f736acd503071097cbeced05cb96361
Pavan <jordan.pac97@gmail.com>
2024-08-26 01:29:21 -0400

    minor changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const cors = require('cors');
+app.use(cors());
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
@@ -19,7 +20,6 @@ const user = require("./routes/user");
 // Using Routes
 app.use("/api/v1", post);
 app.use("/api/v1", user);
-app.use(cors());
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
